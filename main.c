3ed8896c6e22c329377b1c4b212583025a298b2b
itchyny <itchyny@hatena.ne.jp>
2017-01-03 19:17:20 +0900

    add main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "lex.yy.h"
+#include "y.tab.h"
+int yyparse();
+
+int main(void)
+{
+  yyscan_t scanner;
+  yylex_init(&scanner);
+  yyset_in(stdin, scanner);
+  if (yyparse(scanner)) {
+    fprintf(stderr, "Error!\n");
+    yylex_destroy(scanner);
+    exit(1);
+  }
+  yylex_destroy(scanner);
+}
